464b4196cf79e143d607a0d12ad6a00feff9c37b
koe <koe@yelp.com>
2015-08-14 01:55:41 +0100

    Remove aws cli dependency and reorganize the code in functions

--- getvariables.rb
+++ getvariables.rb
@@ -1,92 +1,113 @@
 #!/usr/bin/ruby
 require 'json'
+require 'aws-sdk'
 
+# Clean AWS environment variables.
 ENV.delete('AWS_ACCESS_KEY_ID')
 ENV.delete('AWS_SECRET_ACCESS_KEY')
 
-profiles = []
-File.open(File.expand_path('~/.aws/credentials'), 'r') do |f|
-  f.each_line do |l|
-    next unless l.gsub!(/^\[\s*(\w+)\s*\].*/, '\1')
-    l.chomp!
-    next if l == 'default'
-    profiles.push(l)
-  end
-end
-
-primary_azs = {}
-secondary_azs = {}
-tertiary_azs = {}
-az_counts = {}
-az_lists = {}
-az_letters = {}
-
-data = profiles.map do |account|
-  regions_json = `aws ec2 describe-regions --output json --profile #{account} --region us-east-1`
-  if $?.exitstatus != 0
-    print "Failed to run aws ec2 describe-regions --output json --profile #{account} --region us-east-1"
-    exit 1
-  end
-  regions = JSON.parse(regions_json)['Regions'].map { |d| d['RegionName'] }
-  regions.map do |region|
-    azs_json = `aws ec2 describe-availability-zones --output json --profile #{account} --region #{region}`
-    if $?.exitstatus != 0
-      print "Failed to run aws ec2 describe-availability-zones --output json --profile #{account} --region #{region}"
-      exit 1
+def get_aws_profiles
+# Read all the available profile.
+# For safety reasons 'default' profile will be skipped.
+  profiles = []
+  File.open(File.expand_path('~/.aws/credentials'), 'r') do |f|
+    f.each_line do |l|
+      next unless l.gsub!(/^\[\s*(\w+)\s*\].*/, '\1')
+      l.chomp!
+      next if l == 'default'
+      profiles.push(l)
     end
-    JSON.parse(azs_json)['AvailabilityZones'].map do |tuple|
-      tuple[:name] = "#{account}-#{tuple['RegionName']}"
-      tuple[:sortkey] = "#{account}-#{tuple['ZoneName']}"
-      tuple
-    end
-  end.flatten
-end.flatten.reject { |tuple| tuple['State'] != 'available' }.sort do |a,b|
-  a[:sortkey] <=> b[:sortkey]
+  end
+  profiles
 end
 
-data.each do |tuple|
-  az_counts[tuple[:name]] ||= 0
-  az_counts[tuple[:name]] = az_counts[tuple[:name]]+1
-  az_lists[tuple[:name]] ||= []
-  az_lists[tuple[:name]].push tuple['ZoneName']
-  az_letters[tuple[:name]] ||= []
-  az_letters[tuple[:name]].push tuple['ZoneName'][-1,1]
-  if !primary_azs[tuple[:name]]
-    primary_azs[tuple[:name]] = tuple['ZoneName']
-  elsif !secondary_azs[tuple[:name]]
-    secondary_azs[tuple[:name]] = tuple['ZoneName']
-  elsif !tertiary_azs[tuple[:name]]
-    tertiary_azs[tuple[:name]] = tuple['ZoneName']
+def get_AZs_by_profile account
+  credentials = Aws::SharedCredentials.new(profile_name: "#{account}")
+  @ec2 = Aws::EC2::Client.new(credentials: credentials, region: 'us-east-1')
+
+  begin
+    data = []
+    regions = @ec2.describe_regions().data.regions.map { |region| region.region_name}
+
+    regions.each do |region|
+      @ec2 = Aws::EC2::Client.new(credentials: credentials, region: "#{region}")
+      @ec2.describe_availability_zones({ filters: [{ name: 'state', values: ['available']}] }).data.availability_zones.map do |az|
+        data <<  { 'State' => az.state, 'RegionName' => az.region_name, 'ZoneName' => az.zone_name, :name => "#{account}-#{az.region_name}", :sortkey => "#{account}-#{az.zone_name}" }
+      end
+    end
+
+    # Sort the AZs by account
+    data.sort { |a,b| a[:sortkey] <=> b[:sortkey] }
+  rescue StandardError => e
+    puts e.message
   end
 end
 
-[az_lists, az_letters].each do |squash|
-  squash.each_key { |k| squash[k] = squash[k].join ',' }
-end
-az_counts.each_key { |k| az_counts[k] = "#{az_counts[k]}" }
+def build_variables_file data
+  # Output variables
+  primary_azs = {}
+  secondary_azs = {}
+  tertiary_azs = {}
+  az_counts = {}
+  az_lists = {}
+  az_letters = {}
 
-output = {
- "variable" => {
-    "primary_azs" => {
-      "default" => primary_azs
-    },
-    'secondary_azs' => {
-       "default" => secondary_azs
-    },
-    'tertiary_azs' => {
-        "default" => tertiary_azs
-    },
-    'list_all' => {
-        'default' => az_lists
-    },
-    'list_letters' => {
-        'default' => az_letters
-    },
-    'az_counts' => {
-        'default' => az_counts
-    }
+  data.each do |tuple|
+    az_counts[tuple[:name]] ||= 0
+    az_counts[tuple[:name]] = az_counts[tuple[:name]]+1
+    az_lists[tuple[:name]] ||= []
+    az_lists[tuple[:name]].push tuple['ZoneName']
+    az_letters[tuple[:name]] ||= []
+    az_letters[tuple[:name]].push tuple['ZoneName'][-1,1]
+    if !primary_azs[tuple[:name]]
+      primary_azs[tuple[:name]] = tuple['ZoneName']
+    elsif !secondary_azs[tuple[:name]]
+      secondary_azs[tuple[:name]] = tuple['ZoneName']
+    elsif !tertiary_azs[tuple[:name]]
+      tertiary_azs[tuple[:name]] = tuple['ZoneName']
+    end
+  end
+
+  [az_lists, az_letters].each do |squash|
+    squash.each_key { |k| squash[k] = squash[k].join ',' }
+  end
+  az_counts.each_key { |k| az_counts[k] = "#{az_counts[k]}" }
+
+  output = {
+      "variable" => {
+          "primary_azs" => {
+              "default" => primary_azs
+          },
+          'secondary_azs' => {
+              "default" => secondary_azs
+          },
+          'tertiary_azs' => {
+              "default" => tertiary_azs
+          },
+          'list_all' => {
+              'default' => az_lists
+          },
+          'list_letters' => {
+              'default' => az_letters
+          },
+          'az_counts' => {
+              'default' => az_counts
+          }
+      }
   }
-}
 
-File.open('variables.tf.json.new', 'w') { |f| f.puts JSON.pretty_generate(output) }
-File.rename 'variables.tf.json.new', 'variables.tf.json'
+  begin
+    File.open('variables.tf.json.new', 'w') { |f| f.puts JSON.pretty_generate(output) }
+    File.rename 'variables.tf.json.new', 'variables.tf.json'
+  rescue StandardError => e
+    puts e.message
+  end
+end
+
+# RUN RUN
+variables = []
+get_aws_profiles.each { |account| variables += get_AZs_by_profile account }
+build_variables_file variables
+
+
+
