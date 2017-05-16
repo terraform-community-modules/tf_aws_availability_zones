8d2b4b164d9e114d0c2bee4f49e72b4e8a02e68b
Daniel Schopper <scream314@gmail.com>
2017-05-16 16:18:30 +0200

    removed extra quotation marks

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ variable "region" {}
 variable "account" {}
 
 output "primary" {
-    value = "${lookup(var.primary_azs, format(""\"%s-%s\""", var.account, var.region))}"
+    value = "${lookup(var.primary_azs, format("\"%s-%s\"", var.account, var.region))}"
 }
 output "secondary" {
-    value = "${lookup(var.secondary_azs, format(""\"%s-%s\""", var.account, var.region))}"
+    value = "${lookup(var.secondary_azs, format("\"%s-%s\"", var.account, var.region))}"
 }
 output "tertiary" {
     value = "${lookup(var.tertiary_azs, format("\"%s-%s\"", var.account, var.region))}"
